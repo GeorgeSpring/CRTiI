3be818cef511a891f00a6d42a10913e3f64fc1f2
Юрий Яровой <yarovoy.yura@gmail.com>
2018-07-09 12:28:51 +0400

    Внесены некоторые изменения

---
+++ ЦРТиИ/ЦРТиИ.Android/ScrollableTabbedPageRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms.Platform.Android.AppCompat;
+using Android.Support.Design.Widget;
+using Xamarin.Forms;
+using ЦРТиИ.Droid;
+
+[assembly: ExportRenderer(typeof(ScrollableTabbedPage), typeof(ScrollableTabbedPageRenderer))]
+namespace ЦРТиИ.Droid
+{
+    public class ScrollableTabbedPageRenderer : TabbedPageRenderer
+    {
+        public override void OnViewAdded(Android.Views.View child)
+        {
+            base.OnViewAdded(child);
+            var tabLayout = child as TabLayout;
+            if (tabLayout != null)
+            {
+                tabLayout.TabMode = TabLayout.ModeScrollable;
+            }
+        }
+
+    }
+    public class ScrollableTabbedPage : TabbedPage
+    {
+    }
+}
+
---
+++ ЦРТиИ/ЦРТиИ/Views/ComptetetionListView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ЦРТиИ.Models;
+
+namespace ЦРТиИ.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ComptetetionListView : ContentPage
+	{
+		public ComptetetionListView (List<Competition> Source)
+		{
+			InitializeComponent ();
+           CompetitionsListVie.ItemTapped += async (object sender, ItemTappedEventArgs e) =>
+           {
+               if (e == null) return;
+               await Navigation.PushAsync(new CompetetionDetail(CompetitionsListVie.SelectedItem as Competition));// de-select the row
+               ((ListView)sender).SelectedItem = null;
+           };
+#if __ANDROID__
+            Icon = null;
+#endif
+            CompetitionsListVie.ItemsSource = Source;
+        }
+	}
+}
